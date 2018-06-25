d0c4bd405db2a14d95859033beb6b9424d68ed03
Jaden Lowings <jadenlowings@Jadens-MacBook-Air.local>
2018-06-25 09:54:47 +0200

    GNL v2

--- get_next_line.c
+++ get_next_line.c
@@ -1,91 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jlowing <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/08 11:12:28 by jlowing           #+#    #+#             */
-/*   Updated: 2018/06/20 11:53:11 by jlowing          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-int		ft_malloc(char **check)
+static char	*readline(const int fd, char *buff, int *ret)
 {
-	char	*temporary;
+	char	tmp[BUFF_SIZE + 1];
+	char	*tmp2;
 
-	if (!(temporary = ft_strnew(ft_strlen(*check))))
-		return (0);
-	ft_strcpy(temporary, *check);
-	*check = NULL;
-	if (!(*check = ft_strnew(ft_strlen(temporary) + BUFF_SIZE)))
-		return (0);
-	ft_strcpy(*check, temporary);
+	*ret = read(fd, tmp, BUFF_SIZE);
+	tmp[*ret] = '\0';
+	tmp2 = buff;
+	if (!(buff = ft_strjoin(buff, tmp)))
+		return (NULL);
+	ft_strdel(&tmp2);
+	return (buff);
+}
+
+int			ft_cpy_end(char **line, char **buff)
+{
+	if (!(*line = ft_strdup(*buff)))
+		return (-1);
+	ft_bzero(*buff, ft_strlen(*buff));
 	return (1);
 }
 
-int		fill(int j, char **check, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	int		i;
-	int		y;
+	static char		*buff = "";
+	int				ret;
+	char			*str;
 
-	y = 0;
-	i = 0;
-	if (j != 0 || ft_strlen((*check)) != 0)
-	{
-		while ((*check)[i] != '\n' && (*check)[i])
-		{
-			(*line)[i] = (*check)[i];
-			i++;
-		}
-		(*line)[i++] = '\0';
-/*		while ((*check)[i])
-			(*check)[y++] = (*check)[i++];
-		(*check)[y] = '\0';*/
-		if ((*check)[0] == '\n')
-		{
-			while ((*check)[i])
-				(*check)[y++] = (*check)[i++];
-			(*check)[y] = '\0';
-			return (2);
-		}
-		while ((*check)[i])
-            (*check)[y++] = (*check)[i++];
-        (*check)[y] = '\0';
-	}
-	return (0);//(1)
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	int			j;
-	static char	*check;
-
-	if (fd < 0 || (!check && !(check = ft_strnew(BUFF_SIZE))))
+	ret = 1;
+	if (!line || fd < 0 || (buff[0] == '\0' && (!(buff = ft_strnew(0)))))
 		return (-1);
-	if (!(*line = (char *)malloc(sizeof(char) * BUFF_SIZE)))
-		return (-1);
-	while ((j = read(fd, *line, BUFF_SIZE)) > 0)
+	while (ret > 0)
 	{
-		if (!(ft_malloc(&check)))
+		if ((str = ft_strchr(buff, '\n')) != NULL)
+		{
+			*str = '\0';
+			if (!(*line = ft_strdup(buff)))
+				return (-1);
+			ft_memmove(buff, str + 1, ft_strlen(str + 1) + 1);
+			return (1);
+		}
+		if (!(buff = readline(fd, buff, &ret)))
 			return (-1);
-		ft_strncat(check, *line, BUFF_SIZE);
-		if (ft_memchr(*line, '\n', BUFF_SIZE))
-			break ;
 	}
-	if ((fill(j, &check, &(*line))) == 2)
-	{
-		// write(1, "this", 4);
-		return (1);
-	}
-	// write(1, "this", 4);
-	if (ft_memcmp((*line), check, ft_strlen(*line)) == 0)
-	{
-		if (!(*line = ft_strdup("")))
-			return (0);
-		return (0);
-	}
-	return (1);
+	ft_strdel(&str);
+	if (ret == 0 && ft_strlen(buff))
+		ret = ft_cpy_end(&(*line), &buff);
+	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jlowing <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/08 11:12:41 by jlowing           #+#    #+#             */
-/*   Updated: 2018/06/20 09:26:56 by jlowing          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef get_next_line_h
+#define get_next_line_h
+#include <unistd.h>
+#include "libft.h"
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include "libft.h"
+#define	BUFF_SIZE 42
 
-# define BUFF_SIZE 42
-
-int		get_next_line(const int fd, char **line);
+int		get_next_line(int fd, char **line);
 
 #endif
+
